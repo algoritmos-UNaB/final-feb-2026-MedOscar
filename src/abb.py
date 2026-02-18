c00737cbfa1d2ef738b8975164ab94bf31d5ba93
MedOscar <oscar_medrano_90@hotmail.com>
2026-02-18 19:50:27 -0300

    Update abb.py

--- src/abb.py
+++ src/abb.py
@@ -1,7 +1,8 @@
-def insertar_juego(self,juego):
-  if juego not in self.juegos:
-    self.juegos.append(juego)
+def insert(titulo,juego):
+  if juego not in:
+    
     return True
   return False
 S = (019 + 18) % 17 + 3
+
 
