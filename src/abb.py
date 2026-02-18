e846fc9b5654467586411e279c39624dcb4e649c
MedOscar <oscar_medrano_90@hotmail.com>
2026-02-18 20:03:35 -0300

    Update abb.py

--- src/abb.py
+++ src/abb.py
@@ -6,4 +6,9 @@ def insert(titulo,juego):
 S = (019 + 18) % 17 + 3
 m = 6 + (S % 5)
 
+def search(self,juego):
+  if self.cabeza == juego:
+    return True
+  else:
+    return search(self.hijo_d) search(hijo_i)
 
